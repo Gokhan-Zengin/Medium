a7e94275ca47c4ad95b1bd8dfc743dff423c0983
Gokhan Zengin <ismgokzen@gmail.com>
2022-06-26 18:54:38 +0300

    C# Extension Methods

---
+++ C# Extension Methods/C# Extension Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int value = 5;
+        value.Add(6);
+        Console.WriteLine(value);
+        // Output : 11
+    }
+}
+
+public static class ExtensionMethods
+{
+    public static void Add(this ref int value1, int value2)
+    {
+        value1 += value2;
+    }
+}
